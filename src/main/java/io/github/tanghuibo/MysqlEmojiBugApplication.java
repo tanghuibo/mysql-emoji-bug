783b8347401e7b0402a97a95844da99074eec4f4
tang_huibo <1215893264@qq.com>
2020-06-08 23:44:57 +0800

    add basic

---
+++ src/main/java/io/github/tanghuibo/MysqlEmojiBugApplication.java
@@ -0,0 +1,12 @@
+package io.github.tanghuibo;
+
+/**
+ * @author tanghuibo
+ * @date 2020/6/8下午11:43
+ */
+public class MysqlEmojiBugApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
